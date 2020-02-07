88f2503ef3f109a2c5bbf1b9b3941318d2f6ccf5
Zappy <kittipot1999@hotmail.com>
2020-02-07 17:54:49 +0700

    1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -16,5 +16,30 @@ int main()
     cout << "\nMin = " << B[3];
     return 0;
 }
-
-//Write definition of stat() here 
+void stat(const double A[],int N,double B[])
+{
+	double x=0;
+	double y=0;
+	double z=0;
+	B[3] = A[0];
+	B[2] = A[0];
+	for(int i=0;i<N;i++)
+	{
+		x = x+A[i];
+		B[0] = x/N;
+	}
+	for(int j=0;j<N;j++)
+	{
+		y = y+A[j]*A[j];
+		z = B[0]*B[0];
+		B[1] = pow((1.0/N*y)-z,0.5);
+	}
+	for(int mx=0;mx<N;mx++)
+	{
+		if(A[mx] > B[2]) B[2] = A[mx];
+	}
+	for(int mn=0;mn<N;mn++)
+	{
+		if(A[mn] < B[3]) B[3] = A[mn];
+	}
+}
